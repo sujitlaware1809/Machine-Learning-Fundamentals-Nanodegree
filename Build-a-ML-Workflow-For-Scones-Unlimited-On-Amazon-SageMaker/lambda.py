51663f04a3d4f522a416367f98191b9fb2c3e269
Sujit Laware <148531814+sujitlaware1809@users.noreply.github.com>
2024-09-21 18:37:24 +0530

    Add files via upload

---
+++ Build-a-ML-Workflow-For-Scones-Unlimited-On-Amazon-SageMaker/lambda.py
@@ -0,0 +1,88 @@
+# serialize
+
+import json
+import boto3
+import base64
+import sagemaker
+from sagemaker.serializers import IdentitySerializer
+
+s3 = boto3.client('s3')
+
+def lambda_handler(event, context):
+    """A function to serialize target data from S3"""
+
+    # Get the s3 address from the Step Function event input
+    key = event["s3_key"]
+    bucket = event["s3_bucket"]
+
+    # Download the data from s3 to /tmp/image.png
+    boto3.resource('s3').Bucket(bucket).download_file(key, "/tmp/image.png")
+
+    # We read the data from a file
+    with open("/tmp/image.png", "rb") as f:
+        image_data = base64.b64encode(f.read())
+
+    # Pass the data back to the Step Function
+    print("Event:", event.keys())
+    return {
+        'statusCode': 200,
+        'body': {
+            "image_data": image_data,
+            "s3_bucket": bucket,
+            "s3_key": key,
+            "inferences": []
+        }
+    }
+
+
+# classification
+
+# Fill this in with the name of your deployed model
+ENDPOINT = "image-classification-2024-09-19-06-02-30-978"
+
+def lambda_handler(event, context):
+
+    # Decode the image data
+    image = base64.b64decode(event["image_data"])
+
+    # Instantiate a Predictor
+    predictor = sagemaker.predictor.Predictor(ENDPOINT)
+
+    # For this model the IdentitySerializer needs to be "image/png"
+    predictor.serializer = IdentitySerializer("image/png")
+
+    # Make a prediction:
+    inferences = predictor.predict(image)
+
+    # We return the data back to the Step Function    
+    event["inferences"] = inferences.decode('utf-8')
+    return {
+        'statusCode': 200,
+        'body': json.dumps(event)
+    }
+
+
+# low-confidence inferences
+
+THRESHOLD = .93
+
+
+def lambda_handler(event, context):
+
+    # Grab the inferences from the event
+    inferences = event["inferences"]
+
+    # Check if any values in our inferences are above THRESHOLD
+    meets_threshold = max(inferences) > THRESHOLD
+
+    # If our threshold is met, pass our data back out of the
+    # Step Function, else, end the Step Function with an error
+    if meets_threshold:
+        pass
+    else:
+        raise("THRESHOLD_CONFIDENCE_NOT_MET")
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(event)
+    }
